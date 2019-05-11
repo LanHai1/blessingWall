5448ee95fd318f75a5f6126365c6b7f6e0f012dc
lanhai <1444133004@qq.com>
2019-05-11 13:09:41 +0800

    添加注释

--- 幸福墙案例.js
+++ 幸福墙案例.js
@@ -1,7 +1,9 @@
+// 根据id获取元素
 function $(id) {
     return document.getElementById(id)
 }
 
+// 首次加载数据
 var messages = [{
     "id": 1,
     "name": "张学友",
@@ -51,7 +53,9 @@ var messages = [{
 
 // id处理 => 无数据库 从100开始
 let id = getLocalStorage("id")
+    // id是否已存在本地 默认100开始
 id = id ? id : 100
+    // 存储id
 setLocalStorage("id", id)
 
 // 获取留言数据
@@ -75,12 +79,13 @@ function removeLocalStorage(key) {
     localStorage.removeItem(key)
 }
 
-//找到content
+// 找到 content 元素
 var content = document.getElementById('content');
 
 // 数据存储 单条
 let zIndex = 1;
 
+// 为祝福墙贴纸做对应的事件与绑定
 function setMessageCreateNew(data) {
     let tip = document.createElement("div")
 
@@ -99,19 +104,20 @@ function setMessageCreateNew(data) {
         '<div class="name">' + obj.name + '</div>' +
         '</div>';
 
+    // 将元素追加到对应的父盒子中
     content.appendChild(tip)
         // 随机设置位置
     setElXY(tip)
         // 删除留言板
     tip.getElementsByClassName("close")[0].onclick = function() {
+            // 
             removeTip(obj)
+                // 找到当前留言 并通过父盒子删除该元素
             this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)
         }
         // 双击删除
     tip.getElementsByClassName("tip-h")[0].ondblclick = function() {
-            console.log(111)
             removeTip(obj)
-            console.log(this.parentNode.parentNode)
             this.parentNode.parentNode.removeChild(this.parentNode)
         }
         // z-index
@@ -122,13 +128,19 @@ function setMessageCreateNew(data) {
 
 // 删除留言板
 function removeTip(obj) {
+    // 获取所有数据
     let datas = getLocalStorage("data")
+        // 循环遍历
     for (let i = 0; i < datas.length; i++) {
+        // 通过id 找 对应的数据对象
         if (datas[i]["id"] == obj.id) {
+            // 删除该数据
             datas.splice(i, 1)
+                // 直接跳出循环
             break
         }
     }
+    // 将处理好的新数据 重新设置回本地存储里
     setLocalStorage("data", datas)
 
 }
@@ -151,10 +163,8 @@ function randomXY(max, min) {
     return parseInt(Math.random() * (max - min + 1)) + "px"
 }
 
-// 获取事件 动态添加数据 然后 将数据本地储存  鼠标可移动留言条
-
 let flagDTClose = true
-    // 动态添加数据
+    // 动态添加数据层
 $("addMessage").onclick = function() {
     this.style.display = flagDTClose ? "none" : "block"
     $("submit_con").style.display = flagDTClose ? "block" : "none"
@@ -167,7 +177,7 @@ function closeDT() {
     $("submit_con").style.display = flagDTClose ? "block" : "none"
     flagDTClose = true
 }
-
+// 关闭
 $("closeDT").onclick = function() {
     closeDT()
 }
@@ -179,9 +189,11 @@ $("submit").onclick = function() {
 
 // 数据处理
 function setMessage() {
+    // 获取用户输入
     let text_con = $("text_con").value
     let name_con = $("name_con").value
     let date = getDate(new Date())
+        // 判读是否输入
     if (text_con.length == 0 || name_con.length == 0) {
         $("errSpan").innerHTML = "请填写留言信息和留言人"
         return false
@@ -190,13 +202,16 @@ function setMessage() {
     let data = getLocalStorage("data")
         // 用户输入的单条数据
     let idOne = getLocalStorage("id") + 1
+        // id重新+1 
     setLocalStorage("id", idOne)
+        // 单条数据对象
     let oneData = {
-        "id": idOne,
-        "name": name_con,
-        "content": text_con,
-        "time": date
-    }
+            "id": idOne,
+            "name": name_con,
+            "content": text_con,
+            "time": date
+        }
+        // 将数据添加到原数据的最后面
     data.push(oneData)
 
     // 保存本地
